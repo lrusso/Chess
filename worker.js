89fbe89d0e8a65971da49e8bea31e651cac98608
Leonardo Russo <info@lrusso.com>
2020-08-29 16:27:11 -0300

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,32 @@
+const filesToCache = [
+	"Chess.htm",
+	"Chess.json",
+	"Chess.png",
+	"ChessFavIcon_16x16.png",
+	"ChessFavIcon_192x192.png",
+	"ChessFavIcon_512x512.png"
+];
+
+const staticCacheName = "chess-v1";
+
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(staticCacheName)
+		.then(cache => {
+			return cache.addAll(filesToCache);
+		})
+	);
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request)
+		}).catch(error => {
+		})
+	);
+});
